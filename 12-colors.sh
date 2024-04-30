99900490479fab8f5b54376f209edbf309ea94eb
prabhaks7 <sakereprabhakar@gmail.com>
2024-04-30 09:54:29 +0530

    colors

---
+++ 12-colors.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/temp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+echo "script started executing at: $TIMESTAMP"
+
+VALIDATE(){
+    if [ $1 -ne 0]
+    then
+        echo -e "$2....$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N"
+    fi        
+}
+
+if [ $USERID -ne 0 ]
+then
+  echo "Please run this script with root aaccess"
+  exit 1  # manually exit if error comes.
+else
+  echo "You are super user." 
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MYSQL" 
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing GIT" 
