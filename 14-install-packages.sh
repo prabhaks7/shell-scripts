ef9154ffba7515f106ca1b0f2e10c17da8723fc6
prabhaks7 <sakereprabhakar@gmail.com>
2024-04-30 17:41:06 +0530

    functions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -4,14 +4,18 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2....FAILURE"
+        echo -e "$2....$R FAILURE $N"
         exit 1
     else
-        echo "$2....SUCCESS"
+        echo -e "$2....$G SUCCESS $N"
     fi        
 }
 
@@ -29,8 +33,9 @@ do
   dnf list installed $i &>>$LOGFILE
   if [ $? -eq 0 ]
   then
-      echo "$i already installed....SKIPPING"
+      echo -e "$i already installed...$Y SKIPPING $N"
    else
-      echo "$i not installed.....NEED TO INSTALL"   
+      dnf install $i -y &>>$LOGFILE
+      VALIDATE $? "installation of $i"   
   fi
 done
